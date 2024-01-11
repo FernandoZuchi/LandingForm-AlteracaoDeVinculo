01f7475c9bfcdbd7d07b8e74e5b38f5a4de6962c
FernandoZuchi <nandozuchi07@gmail.com>
2024-01-10 21:52:40 -0300

    PopUp quase concluido, dinâmica e envio de emails funcionando, falta apenas finalizações no css

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,6 @@ const submitBtn = document.querySelector('.submit-btn'),
     email = document.querySelector('#mail'),
     errorDisplayers = document.getElementsByClassName('error'),
     cardContainer = document.querySelector('.card-container'),
-    outroOverlay = document.querySelector('.outro-overlay'),
     form = document.querySelector('form')
 
 // Função para validar e-mail
@@ -13,10 +12,24 @@ function isValidEmail(email) {
     return emailRegex.test(email);
 }
 
+// Adicionar um event listener para o botão de fechar o modal
+const closeModalBtn = document.getElementById('close-modal-btn');
+closeModalBtn.addEventListener('click', () => {
+    const successModal = document.getElementById('success-modal');
+    successModal.style.display = 'none';
+
+    // Limpar o formulário
+    form.reset();
+
+    // Recarregar a página
+    location.reload();
+});
+
+
 // Event listener para o botão de envio
 form.addEventListener('submit', async (e) => {
     e.preventDefault()
-    
+
     // Limpar mensagens de erro
     for (const errorDisplayer of errorDisplayers) {
         errorDisplayer.textContent = '';
@@ -37,8 +50,13 @@ form.addEventListener('submit', async (e) => {
     }
 
     const startDate = document.querySelector('#start-date').value;
-    if (!startDate) {
-        errorDisplayers[3].textContent = 'A data de início do curso não permite trancamento.';
+    const today = new Date();
+    const lastMonthOfCourse = new Date(startDate);
+    lastMonthOfCourse.setMonth(lastMonthOfCourse.getMonth() + 11);
+    
+    // Verificar se a data de início está dentro do último mês
+    if (today >= lastMonthOfCourse && today < new Date(startDate).setMonth(lastMonthOfCourse.getMonth() + 1)) {
+        errorDisplayers[3].textContent = 'Você não pode trancar o curso no último mês.';
         isValid = false;
     }
 
@@ -53,13 +71,8 @@ form.addEventListener('submit', async (e) => {
             if (response.ok) {
                 // Exibir o modal
                 const successModal = document.getElementById('success-modal');
+                successModal.classList.remove('disabled');
                 successModal.style.display = 'flex';
-
-                // Adicionar um botão para fechar o modal
-                const closeModalBtn = document.getElementById('close-modal-btn');
-                closeModalBtn.addEventListener('click', () => {
-                    successModal.style.display = 'none';
-                });
             } else {
                 console.error('Erro ao enviar e-mail.');
             }
